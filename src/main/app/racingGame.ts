47692e1618ba18cd59dec4bbee9dee2d103d57a3
moonheekim0118 <hellomooneekim@gmail.com>
2024-01-27 21:26:09 +0900

    <feat> 자동차의 입력을 입력 받는다.

--- src/main/app/racingGame.ts
+++ src/main/app/racingGame.ts
@@ -1,13 +1,14 @@
+import Car from "../domain/car";
 import InputView from "../view/inputView";
 
 class RacingGameApp {
   readonly inputView;
-  readonly Cars: [];
+  Cars: [];
 
   constructor() {
     this.inputView = new InputView();
     this.inputView.inputCarNames().then((result) => {
-      console.log(result);
+      this.Cars = result.split(",").map((car) => new Car(car));
     });
   }
 }
